bcbfb7526467cfc49c47995af939e5512d22f398
Sujal Dhungana <sujal.dhungana@watercorporation.com.au>
2022-07-13 20:10:42 +0800

    let's commit the changes my feature is complete

--- 02_to_10_vscode_extensions/Code/test.py
+++ 02_to_10_vscode_extensions/Code/test.py
@@ -13,3 +13,8 @@ print('For session 02 I am going to do this demo')
 # Let's see how we can track the history of this file
 
 print('This is the second change')
+
+
+# this part will the updated while in the branch and then merged to main
+
+print('This is the third change. This change is made in the feature branch')
