d9fe69c766cb0a82bb8ff3cc35c14f04308d6892
Sujal Dhungana <sujal.dhungana@watercorporation.com.au>
2022-07-13 20:06:06 +0800

    creating some history for the file

--- 02_to_10_vscode_extensions/Code/test.py
+++ 02_to_10_vscode_extensions/Code/test.py
@@ -6,3 +6,10 @@ print("Hello world")
 # This has to be my next change
 
 print('For session 02 I am going to do this demo')
+
+
+
+
+# Let's see how we can track the history of this file
+
+print('This is the second change')
